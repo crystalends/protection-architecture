24752e9373527ca393657b6502a714c0b1148f4c
crystalends <shimko.alex.fereline@gmail.com>
2025-08-28 13:01:31 +0300

    feat: fix header

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,18 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu');
 
     burgerBtn.addEventListener('click', () => {
-        menu.classList.toggle('active');
+        menu.classList.toggle('menu--active');
 
-        if (menu.classList.contains('active')) {
+        if (menu.classList.contains('menu--active')) {
             document.body.style.overflow = 'hidden';
         } else {
             document.body.style.overflow = '';
         }
     });
-    
+
     document.addEventListener('click', (e) => {
         if (!menu.contains(e.target) && !burgerBtn.contains(e.target)) {
-            menu.classList.remove('active');
+            menu.classList.remove('menu--active');
             document.body.style.overflow = '';
         }
     });
